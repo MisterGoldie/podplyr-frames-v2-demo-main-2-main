622b4d41dc281017201a36959b8407b3f2ae2c9f
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-02-09 15:45:06 -0600

    COMMIT BEFORE BG CHANGE

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -33,6 +33,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export { app };
 export const db = getFirestore(app);
 
 // Track user search and return Farcaster user data
