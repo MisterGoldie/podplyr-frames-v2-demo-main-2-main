b0f14fa608f063c35d416ba73ab1662f827d02b8
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-02-10 23:08:15 -0600

    TRYING TO COMMIT

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -1,10 +1,14 @@
 import { FrameNotificationDetails } from "@farcaster/frame-sdk";
 import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
-});
+let redis: Redis | null = null;
+
+if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+  redis = new Redis({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+  });
+}
 
 function getUserNotificationDetailsKey(fid: number): string {
   return `POD PLAYR:user:${fid}`;
@@ -13,6 +17,7 @@ function getUserNotificationDetailsKey(fid: number): string {
 export async function getUserNotificationDetails(
   fid: number
 ): Promise<FrameNotificationDetails | null> {
+  if (!redis) return null;
   return await redis.get<FrameNotificationDetails>(
     getUserNotificationDetailsKey(fid)
   );
@@ -22,11 +27,13 @@ export async function setUserNotificationDetails(
   fid: number,
   notificationDetails: FrameNotificationDetails
 ): Promise<void> {
+  if (!redis) return;
   await redis.set(getUserNotificationDetailsKey(fid), notificationDetails);
 }
 
 export async function deleteUserNotificationDetails(
   fid: number
 ): Promise<void> {
+  if (!redis) return;
   await redis.del(getUserNotificationDetailsKey(fid));
 }
