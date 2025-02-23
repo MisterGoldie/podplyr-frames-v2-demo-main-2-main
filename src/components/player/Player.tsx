a0c1e45b65d452d280d1af221db8ae952f34dbb5
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-02-23 10:14:29 -0600

    FIXED BROKEN X ON INFO CARD

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -392,7 +392,7 @@ export const Player: React.FC<PlayerProps> = ({
               aria-label="Close info panel"
             >
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor">
-                <path d="M256-200-56-56 224-224-224-224 56-56 224 224 224-224-56 56-224 224-224 224Z"/>
+                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
               </svg>
             </button>
           </div>
