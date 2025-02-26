1712fca678ef4fab126fc4639cc05a988d6e5224
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-02-26 02:13:09 -0600

    X BUTTON UPDATE

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -420,7 +420,7 @@ export const Player: React.FC<PlayerProps> = ({
               aria-label="Close info panel"
             >
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor">
-                <path d="M256-200-56-56 224-224-224-224 56-56 224 224 224-224-56 56-224 224-224 224Z"/>
+                <path d="M480-424 284-228q-11 11-28 11t-28-11q-11-11-11-28t11-28l196-196-196-196q-11-11-11-28t11-28q11-11 28-11t28 11l196 196 196-196q11-11 28-11t28 11q11 11 11 28t-11 28L536-480l196 196q11 11 11 28t-11 28q-11 11-28 11t-28-11L480-424Z"/>
               </svg>
             </button>
           </div>
