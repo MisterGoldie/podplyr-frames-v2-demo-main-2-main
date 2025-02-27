30e1a61e50e90b996fb5bae388f888e63c8ef4b7
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-02-27 02:36:44 -0600

    LIBRARY PAGE PADDING

--- src/components/views/LibraryView.tsx
+++ src/components/views/LibraryView.tsx
@@ -313,7 +313,7 @@ class LibraryView extends React.Component<LibraryViewProps> {
             </div>
           ) : (
             <div 
-              className={`px-4 pb-48 ${viewMode === 'grid' ? 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4' : 'space-y-4'}`}
+              className={`px-4 pb-32 ${viewMode === 'grid' ? 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4' : 'space-y-4'}`}
             >
               {filteredNFTs.map((nft) => (
                 <SimpleNFTCard
