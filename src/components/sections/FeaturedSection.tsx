e8db9d41b3ccfb24f56f44057c8a3f85690f4f74
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-03-01 22:13:41 -0600

    HEART ICONS CIRCLE REMOVAL

--- src/components/sections/FeaturedSection.tsx
+++ src/components/sections/FeaturedSection.tsx
@@ -178,14 +178,14 @@ const FeaturedSection: React.FC<FeaturedSectionProps> = ({
                   {userFid && parseInt(String(userFid)) > 0 && onLikeToggle && (
                     <button 
                       onClick={() => onLikeToggle(nft)}
-                      className="absolute top-2 right-2 w-8 h-8 rounded-full bg-black/40 text-white flex items-center justify-center transition-all duration-200 hover:scale-110 z-10"
+                      className="absolute top-2 right-2 text-red-500 transition-all duration-200 hover:scale-125 z-10"
                     >
                       {isNFTLiked(nft) ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor" className="text-red-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor">
                           <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"/>
                         </svg>
                       ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor" className="text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor" className="text-white hover:text-red-500">
                           <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
                         </svg>
                       )}
