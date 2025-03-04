d17a1461039571adf9cd0bd70059e73e0f8559d1
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-03-04 13:44:00 -0600

    SHAVED OFF 100MS

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://arweave.net" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://firestore.googleapis.com" crossOrigin="anonymous" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         </Head>
         <Main />
